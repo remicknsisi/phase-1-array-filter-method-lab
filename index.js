369eead87f75020b294a01474c3aa8e300201b83
Sienna <remicknsisi@gmail.com>
2022-09-27 15:23:44 -0400

    commit tree 92d5de8d49fd45f8d3fd76624d881cd3f900ad12

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, str){
+    return drivers.filter(function(driver){
+        return (driver.toLowerCase() === str.toLowerCase())
+    })
+}
+
+function fuzzyMatch(drivers, str){
+    return drivers.filter(function(driver){
+        return driver.substring(0, str.length) === str})
+    }
+
+function matchName(drivers, str){
+    return drivers.filter(function(element){
+        return element.name === str
+    })
+}
